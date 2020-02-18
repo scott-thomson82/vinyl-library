a6154925d0e35902bc34bfaf49bbbd12a62138f6
Scott Thomson <scottthomson_57@hotmail.com>
2020-02-18 15:09:39 +1300

    adding style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Sidebar from './Sidebar'
 
 const App = () => {
   return (
     <>
     <Header />
+    <Sidebar />
 
 
     </>
---
+++ client/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div>
+        <div className="sidebar">
+        <a href="#" className="add">Add</a>
+        <a href="#" className="search">Search</a>
+        <a href="#" className="list">List</a>
+        <a href="#" className="wanted">Wanted</a>
+      </div>
+      </div>
+    )
+}
+
+export default Sidebar
